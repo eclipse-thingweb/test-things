7a76e05e1ba52b67bff771e5b0de52201c6d2dd7
Hasan Eroglu <hasanheroglu@gmail.com>
2024-08-20 14:42:09 +0300

    add advanced-coffee-machine
    Signed-off-by: Hasan Eroglu <hasanheroglu@gmail.com>

---
+++ things/advanced-coffee-machine/http/ts/test/fixtures.ts
@@ -0,0 +1,24 @@
+import { ChildProcess } from "child_process"
+import { getInitiateMain, ThingStartResponse } from "../../../../../util/util"
+import path from "path"
+
+let thingProcess: ChildProcess | undefined
+let response: ThingStartResponse
+const port = 3000
+
+export async function mochaGlobalSetup() {
+    try {
+        response = await getInitiateMain(path.join(__dirname, '..', 'dist', 'main.js'), port)
+        thingProcess = response.process
+    } 
+    catch(error: any) {
+        console.log(error)
+        thingProcess = error.process
+    }
+}
+
+export function mochaGlobalTeardown() {
+    if (thingProcess) {
+        thingProcess.kill()
+    }
+}
