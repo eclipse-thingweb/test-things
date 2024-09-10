c7aa9302eb5069e826f43a188b773b2fca91dbf7
Hasan Eroglu <hasanheroglu@gmail.com>
2024-09-10 17:16:42 +0300

    add mocha tests for coap calculator
    Signed-off-by: Hasan Eroglu <hasanheroglu@gmail.com>

--- things/calculator/coap/js/test/td.test.js
+++ things/calculator/coap/js/test/td.test.js
@@ -1,79 +1,83 @@
-const Ajv = require('ajv')
 const chai = require('chai')
-const https = require('https')
 const coap = require('coap')
-const path = require('path')
-
-const spawn = require('child_process').spawn
-
-const ajv = new Ajv({ strict: false, allErrors: true, validateFormats: false })
+const cbor = require('cbor')
+const { getTDValidate } = require('../../../../../util/dist/util')
+const { simplePort, contentNegotiationPort } = require('./fixtures')
 
 const expect = chai.expect
-const port = 5683
-let thingProcess
 
 describe('Calculator CoAP JS', () => {
   let validate
 
   before(async () => {
-    const initiateMain = new Promise(async (resolve, reject) => {
-      thingProcess = spawn(
-        'node',
-        ['coap-simple-calculator.js', '-p', `${port}`],
-        { cwd: path.join(__dirname, '..') }
-      )
-      thingProcess.stdout.on('data', (data) => {
-        if (data.toString().trim() === 'ThingIsReady') {
-          resolve('Success')
-        }
-      })
-      thingProcess.stderr.on('data', (data) => {
-        reject(`Error: ${data}`)
-      })
-      thingProcess.on('error', (error) => {
-        reject(`Error: ${error}`)
-      })
-      thingProcess.on('close', () => {
-        reject('Failed to initiate the main script.')
-      })
-    })
+    const tdValidate = getTDValidate()
+  
+    try {
+      const response = await Promise.all([tdValidate])
+      validate = response[0].validate
+    } 
+    catch (error) {
+      console.log(error)
+    }
+  })
 
-    const getJSONSchema = new Promise((resolve, reject) => {
-      https.get('https://raw.githubusercontent.com/w3c/wot-thing-description/main/validation/td-json-schema-validation.json', function (response) {
-        const body = []
-        response.on('data', (chunk) => {
-          body.push(chunk)
-        })
-
-        response.on('end', () => {
-          const tdSchema = JSON.parse(Buffer.concat(body).toString())
-          validate = ajv.compile(tdSchema)
-          resolve('Success')
-        })
+  describe('Calculator Simple', () => {
+    it('should have a valid TD', (done) => {
+      const req = coap.request(`coap://localhost:${simplePort}/coap-calculator-simple`)
+  
+      req.on('response', (res) => {
+        const valid = validate(JSON.parse(res.payload.toString()))
+        expect(valid).to.be.true
+        done()
       })
-    })
-
-    await Promise.all([initiateMain, getJSONSchema]).then(data => {
-      if (data[0] !== 'Success' || data[1] !== 'Success') {
-        console.log(`initiateMain: ${data[0]}`)
-        console.log(`getJSONSchema: ${data[1]}`)
-      }
+  
+      req.end()
     })
   })
 
-  after(() => {
-    thingProcess.kill()
-  })
+  describe('Calculator Content Negotiation', () => {
+    it('should have a valid application/json TD', (done) => {
+      const req = coap.request({
+          method: 'GET',
+          observe: false,
+          host: 'localhost',
+          port: contentNegotiationPort,
+          pathname: 'coap-calculator-content-negotiation',
+          headers: {
+              "Accept": 'application/json'
+          }
+      })
+      
+      req.on('response', (res) => {
+        const valid = validate(JSON.parse(res.payload.toString()))
+        expect(valid).to.be.true
+        done()
+      })
 
-  it('should have a valid TD', (done) => {
-    const req = coap.request(`coap://localhost:${port}/coap-calculator-simple`)
-
-    req.on('response', (res) => {
-      const valid = validate(JSON.parse(res.payload.toString()))
-      expect(valid).to.be.true
-      done()
+      req.end()
     })
 
-    req.end()
+    it('should have a valid application/cbor TD', (done) => {
+      const req = coap.request({
+          method: 'GET',
+          observe: false,
+          host: 'localhost',
+          port: contentNegotiationPort,
+          pathname: 'coap-calculator-content-negotiation',
+          headers: {
+              "Accept": 'application/cbor'
+          }
+      })
+      
+      req.on('response', (res) => {
+        // console.log(res.payload.toString())
+        const decodedPayload = cbor.decode(res.payload)
+        const valid = validate(JSON.parse(decodedPayload))
+        expect(valid).to.be.true
+        done()
+      })
+
+      req.end()
+    })
   })
 })
