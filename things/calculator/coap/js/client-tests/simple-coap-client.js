3468cbf06560753fa96e4cc43cd022e96c783acd
Hasan Eroglu <hasanheroglu@gmail.com>
2024-09-27 18:12:25 +0200

    run prettier
    Signed-off-by: Hasan Eroglu <hasanheroglu@gmail.com>

--- things/calculator/coap/js/client-tests/simple-coap-client.js
+++ things/calculator/coap/js/client-tests/simple-coap-client.js
@@ -1,14 +1,29 @@
+/********************************************************************************
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ * Document License (2015-05-13) which is available at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+
 const coap = require("coap");
 const hostname = "localhost";
 const portNumber = 5683;
 const thingName = "coap-calculator-simple";
 
-const fullTDEndpoint = `/${thingName}`,
-    resultEndPoint = `/${thingName}/properties/result`,
-    lastChangeEndPoint = `/${thingName}/properties/lastChange`,
-    additionEndPoint = `/${thingName}/actions/add`,
-    subtractionEndPoint = `/${thingName}/actions/subtract`,
-    updateEndPoint = `/${thingName}/events/update`;
+const fullTDEndpoint = `/${thingName}`;
+const resultEndPoint = `/${thingName}/properties/result`;
+const lastChangeEndPoint = `/${thingName}/properties/lastChange`;
+const additionEndPoint = `/${thingName}/actions/add`;
+const subtractionEndPoint = `/${thingName}/actions/subtract`;
+const updateEndPoint = `/${thingName}/events/update`;
 
 /****************************************/
 /****** Thing Description Endpoint ******/
