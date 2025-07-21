da7b4a713b9e58a86338ddc45f128a68811a995c
sailalithkanumuri8 <saikanbolt@gmail.com>
2025-07-21 16:33:05 -0700

    Initial tracing code

---
+++ things/advanced-coffee-machine/http/ts/src/tracing.ts
@@ -0,0 +1,16 @@
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { JaegerExporter } from "@opentelemetry/exporter-jaeger";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+
+const provider = new NodeTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "advanced-coffee-machine"
+  }),
+});
+const exporter = new JaegerExporter({
+  endpoint: process.env.JAEGER_ENDPOINT || "http://localhost:14268/api/traces"
+});
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+provider.register();
